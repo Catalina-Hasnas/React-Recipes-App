45de71e161f5ffb472524a771ec56015eddbbfba
Catalina Hasnas <catalinahasnas1996@gmail.com>
2020-09-21 15:55:58 +0300

    Updates

---
+++ precache-manifest.f0a5575232f07cd492e5e6576dc08898.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f8c40fe78d9fac6399d",
+    "url": "/React-recipes-app/static/css/main.328ac466.chunk.css"
+  },
+  {
+    "revision": "5f8c40fe78d9fac6399d",
+    "url": "/React-recipes-app/static/js/main.5f8c40fe.chunk.js"
+  },
+  {
+    "revision": "f22b9333da735c2e345e",
+    "url": "/React-recipes-app/static/css/1.b772a9af.chunk.css"
+  },
+  {
+    "revision": "f22b9333da735c2e345e",
+    "url": "/React-recipes-app/static/js/1.f22b9333.chunk.js"
+  },
+  {
+    "revision": "833b18641dfeeba9c04a",
+    "url": "/React-recipes-app/static/js/runtime~main.833b1864.js"
+  },
+  {
+    "revision": "90ceaec128d2b8c470c0bd00b990f2d9",
+    "url": "/React-recipes-app/static/media/avocado-toast.90ceaec1.jpg"
+  },
+  {
+    "revision": "288869c6cb21f121d2c4e818d47187e3",
+    "url": "/React-recipes-app/static/media/greek-salad.288869c6.jpg"
+  },
+  {
+    "revision": "915b5bbab659ac8cbd5590884395641a",
+    "url": "/React-recipes-app/static/media/mushroom-cream-soup.915b5bba.jpg"
+  },
+  {
+    "revision": "016c1a4c82ee773c1eb8d47518f2c3f0",
+    "url": "/React-recipes-app/index.html"
+  }
+];
